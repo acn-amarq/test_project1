44435cbffacb547fb6df5965cf65443bdd1b19d8
Marquez Jr <antonio.d.marquez.jr@accenture.com>
2018-10-31 21:50:41 +0800

    modified commands.bash file

---
+++ commands.bash
@@ -0,0 +1,20 @@
+#UNIX COMMANDS:
+ls 							: list directory
+ls -la						: list directory show all hidden
+mkdir						: create directory
+rm -r <dir>					: delete folder
+rm -rf <dir>				: force delete folder 
+touch <filename>			: create file
+echo "text" >>	<filename>	: append text to file
+cat <filename>				: display text from file
+
+
+#GIT COMMANDS:
+git init						: initialize git in the current folder
+git add <filename>				: Stage change
+git commit -m "commit message"	: Commit change
+git remote add origin <url>		: Add remote repository
+git push -u origin master		: Push change to remote repository
+
+
+
